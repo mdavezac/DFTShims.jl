57d7408ea0b72d914cb48a9557e9d8de9436f47b
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2017-08-04 20:25:40 +0100

    Type-stable polarized Base.zeros overloads

--- src/ArrayInitialization.jl
+++ src/ArrayInitialization.jl
@@ -10,20 +10,36 @@ macro lintpragma(s) end
 
 const DD = Dispatch.Dimensions
 
+""" Creates the tuple of axes for a spin-polarized quantity """
+@generated polarized_axis(comps::Tuple, defaults::Tuple, ax::Tuple) = begin
+    if length(ax.parameters) == length(defaults.parameters)
+        :(defaults)
+    elseif length(ax.parameters) == length(defaults.parameters) - 1
+        :(ax..., Axis{:spin}(comps))
+    else
+        :(Base.front(defaults)..., Axis{:spin}(comps))
+    end
+end
+
 """ Creates an unpolarized array for the given DFT quantity """
-zeros_impl_polarized(T::Type{<:DD.Scalars.All}, dims::Tuple, ax::Tuple) = begin
+Base.zeros(T::Type{<:DD.Scalars.All}, ::Type{Polarized},  dims::Tuple, ax::Tuple) = begin
     length(ax) > (length(dims) + 1) && throw(ArgumentError("Too many axes"))
     comps = components(T, Polarized)
     data = zeros(T, (dims..., length(comps)))
-    if length(ax) == length(dims) + 1
-        AxisArray(data, ax...)
-    else
-        AxisArray(data, AxisArrays.default_axes(data, ax)[1:end - 1]..., Axis{:spin}(comps))
-    end
+    defaults = AxisArrays.default_axes(data, ax)
+    AxisArray(data, polarized_axis(comps, defaults, ax))
 end
-""" Creates a polarized array for the given DFT quantity """
-zeros_impl_unpolarized(T::Type{<:DD.Scalars.All}, dims::Tuple, ax::Tuple) =
+Base.zeros(T::Type{<:DD.Scalars.All}, ::Type{Polarized}, dims::Tuple) = begin
+    comps = components(T, Polarized)
+    data = zeros(T, (dims..., length(comps)))
+    defaults = AxisArrays.default_axes(data)
+    AxisArray(data, Base.front(defaults)..., Axis{:spin}(comps))
+end
+""" Creates an unpolarized array for the given DFT quantity """
+Base.zeros(T::Type{<:DD.Scalars.All}, ::Type{Unpolarized}, dims::Tuple, ax::Tuple) =
     AxisArray(zeros(T, dims), ax...)
+Base.zeros(T::Type{<:DD.Scalars.All}, ::Type{Unpolarized}, dims::Tuple) =
+    AxisArray(zeros(T, dims))
     
     
 @lintpragma("Ignore unused args")
@@ -33,17 +49,11 @@ Creates an array for the given DFT quantity
 The spin components, if any, are added as the last dimension.
 Note should `dims` does not include the spin components.
 """
-Base.zeros(T::Type{<:DD.Scalars.All}, ::Type{Polarized},
+Base.zeros(T::Type{<:DD.Scalars.All}, P::Type{<: PolarizationCategory},
            args::Vararg{<:Union{Integer, Axis}}) = begin
     @lintpragma("Ignore use of undeclared variable x")
-    zeros_impl_polarized(T, ((x for x in args if typeof(x) <: Integer)...),
-                         ((x for x in args if typeof(x) <: Axis)...))
-end
-Base.zeros(T::Type{<:DD.Scalars.All}, ::Type{Unpolarized},
-           args::Vararg{<:Union{Integer, Axis}}) = begin
-    @lintpragma("Ignore use of undeclared variable x")
-    zeros_impl_unpolarized(T, ((x for x in args if typeof(x) <: Integer)...),
-                           ((x for x in args if typeof(x) <: Axis)...))
+    zeros(T, P, ((x for x in args if typeof(x) <: Integer)...),
+            ((x for x in args if typeof(x) <: Axis)...))
 end
 Base.zeros(T::Type{<:DD.Scalars.All}, polarized::Bool,
            args::Vararg{<:Union{Integer, Axis}}) =
--- test/Initialization.jl
+++ test/Initialization.jl
@@ -6,6 +6,7 @@ const DH = DFTShims.Dispatch.Hartree
 const Dρ = DH.Scalars.ρ
 const SIZES = 10, 2
 const AXES = Axis{:radius}(1:SIZES[1]), Axis{:bb}((:α, :β))
+
 @testset "Unpolarized" begin
     ρ = zeros(Dρ{Int64}, false, SIZES...)
     @test typeof(ρ) <: AxisArray{Dρ{Int64}}
@@ -23,9 +24,26 @@ const AXES = Axis{:radius}(1:SIZES[1]), Axis{:bb}((:α, :β))
     @test axes(ρ, 2) == AXES[2]
 
     @test_throws ArgumentError zeros(Dρ{Int64}, Unpolarized, SIZES..., AXES..., AXES[1])
+
+    @inferred zeros(Dρ{Int64}, Unpolarized, SIZES)
+    @inferred zeros(Dρ{Int64}, Unpolarized, SIZES, AXES)
+    @inferred zeros(Dρ{Int64}, Unpolarized, SIZES, AXES[1:1])
 end
 
 @testset "Polarized" begin
+    @testset "axis names" begin
+        comps = components(Dρ{Int64}, Polarized)
+        extra_axis = Axis{:a}((1, 2))
+        spin_axis = Axis{:spin}((:α, :β)) 
+        defaults(a) = AxisArrays.default_axes(zeros((SIZES..., 2)), a)
+        pa(a) = @inferred DFTShims.ArrayInitialization.polarized_axis(comps, defaults(a), a)
+
+        @test pa((AXES..., extra_axis)) == defaults((AXES..., extra_axis))
+        @test pa(AXES) == (AXES..., spin_axis)
+        @test pa(AXES[1:1]) == (defaults(AXES[1:1])[1:end - 1]..., spin_axis)
+        @test pa(AXES[1:0]) == (defaults(AXES[1:0])[1:end - 1]..., spin_axis)
+    end
+
     ρ = zeros(Dρ{Int64}, true, SIZES...)
     @test typeof(ρ) <: AxisArray{Dρ{Int64}}
     @test size(ρ) == (SIZES..., 2)
@@ -50,4 +68,8 @@ end
     @test size(∂³ϵ_∂ρ∂σ²) == (SIZES..., 12)
     @test axes(∂³ϵ_∂ρ∂σ², 1) == AXES[1]
     @test axes(∂³ϵ_∂ρ∂σ², 2) == AXES[2]
+
+    @inferred zeros(Dρ{Int64}, Polarized, SIZES)
+    @inferred zeros(Dρ{Int64}, Polarized, SIZES, AXES)
+    @inferred zeros(Dρ{Int64}, Polarized, SIZES, AXES[1:1])
 end
